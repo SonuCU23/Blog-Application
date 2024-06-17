3e8566176a079d33a1d262d23c0be998a0c3196d
Sonu Ray <sonuray1604@gmail.com>
2024-06-17 11:17:37 +0530

    Exception Handling

--- src/main/java/com/blog/app/controller/UserController.java
+++ src/main/java/com/blog/app/controller/UserController.java
@@ -5,6 +5,8 @@ import com.blog.app.request.UserRequestDto;
 import com.blog.app.response.BaseResponse;
 import com.blog.app.response.UserResponseDto;
 import com.blog.app.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,44 +15,50 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/api/user")
+@Tag(name = "User API", description = "Create and Fetch User Details")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/")
-    public ResponseEntity<BaseResponse> create(@RequestBody UserRequestDto userRequestDto) {
+    @PostMapping("/v1/user")
+    @Operation(description = "This API is used to add user details")
+    public ResponseEntity<BaseResponse> create(@RequestBody @Valid UserRequestDto userRequestDto) {
         UserResponseDto createdUser = this.userService.createUser(userRequestDto);
         BaseResponse response = new BaseResponse("SUCCESS", "User Created Successfully", createdUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{userId}")
+    @Operation(description = "This API is used to update user details by userId")
     public ResponseEntity<BaseResponse> updateUser(@PathVariable("userId") Integer userId, @RequestBody UpdateUserRequestDto updateUserRequestDto) {
         BaseResponse response  = this.userService.updateUser(userId, updateUserRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/v1/user/{userId}")
+    @Operation(description = "This API is used to fetch user details by userId")
     public ResponseEntity<BaseResponse> getUsers(@PathVariable("userId") Integer userId) {
         UserResponseDto fetchedUser = this.userService.getUserById(userId);
         BaseResponse response = new BaseResponse("SUCCESS", "User Fetched Successfully", fetchedUser);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/")
+    @GetMapping("/v1/user")
+    @Operation(description = "This API is used to fetch all users")
     public ResponseEntity<BaseResponse> getAllUsers() {
         List<UserResponseDto> users = this.userService.getAllUsers();
         BaseResponse response = new BaseResponse("SUCCESS", "User Fetched Successfully", users);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping("/v1/user/{userId}")
+    @Operation(description = "This API is used to delete user by userId")
     public ResponseEntity<BaseResponse> deleteUser(@PathVariable("userId") Integer userId) {
         this.userService.deleteUser(userId);
         BaseResponse response = new BaseResponse("SUCCESS", "User Deleted Successfully", "");
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
---
+++ src/main/java/com/blog/app/handler/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.blog.app.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler {
+
+    private static final String INVALID_REQUEST = "Invalid request";
+
+
+
+}
+
